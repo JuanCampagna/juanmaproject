f6f7cca7a0e57321f061ffdfbea6074684f556d5
PixlKing <campagnamatias@gmail.com>
2020-01-24 20:33:24 -0300

    agregado un select

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
 <body>
 	<button>Boton</button>
 	<p>este es el primer titulo</p>
+
+	<select class="mongo">
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+	</select>
 </body>
 </html>
